51c366e633f6a7a0f4ea2133b199379a6ab6e630
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-12-27 15:44:46 -0800

    output: updating output logic to support multi bootstrap
    If this is not put in place, terraform will error with this issue. Currently as of terraform 0.11.11, this is not fixed.
    
    Error: Error running plan: 1 error(s) occurred:
    
    * module.spoke-1.module.dcos-install.module.dcos-bootstrap-install.output.depends: Resource 'null_resource.bootstrap' not found for variable 'null_resource.bootstrap.0.id'

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "depends" {
   description = "Modules are missing the depends_on feature. Faking this feature with input and output variables"
-  value       = "${null_resource.bootstrap.0.id}"
+  value       = "${element(concat(null_resource.bootstrap.*.id, list("")), 0)}"
 }
