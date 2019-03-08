307edf1b505f9c83aca1838826e368b78803a793
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2019-03-08 15:25:52 -0800

    output: remove nil output from terraform module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
 output "depends" {
   description = "Modules are missing the depends_on feature. Faking this feature with input and output variables"
-  value       = "${null_resource.bootstrap.0.id}"
+  value       = "${coalesce(null_resource.bootstrap.0.id, "")}"
+
+  # TODO(mbernadin): remove coalesce when nil on destroy is fixed: https://github.com/hashicorp/terraform/issues/17862
 }
